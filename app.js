32d0a28b038d7fd9a16dc18eabd0f1bf45da5329
Bankole Esan <techybanky@gmail.com>
2020-05-10 19:28:42 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '.app',
+  data: {
+    errorMsg: false,
+    successMsg: false,
+    warningMsg: false,
+    infoMsg: false,
+    showAddModal: false,
+    showEditModal: false,
+    showDeleteModal: false,
+  }
+})
